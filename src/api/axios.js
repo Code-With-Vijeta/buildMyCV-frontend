80ab672155c1a77ea1dc7f96b64ec14143bd691e
Code-With-Vijeta <codevijeta@gmail.com>
2025-11-17 12:00:23 +0530

    updated api in login signup

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`, 
+  baseURL: import.meta.env.VITE_API_URL, // https://buildmycv-backend.onrender.com/api
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  withCredentials: true, // important for cookies
 });
 
 api.interceptors.request.use((config) => {
